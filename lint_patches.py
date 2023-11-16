691b51fad7dc0c6959506b8f54b1c3c3505b122a
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-11-16 14:01:53 +0100

    fix SA

--- lint_patches.py
+++ lint_patches.py
@@ -4,17 +4,17 @@ import os
 from yaml import safe_load
 
 
-def main(path:str) -> None:
-    with open(path, encoding="utf-8") as f:
-        content = f.read()
+def main(path: str) -> int:
+    with open(path, encoding="utf-8") as file:
+        content = file.read()
 
     parsed = safe_load(content)
 
     patches_path = os.path.join(os.path.dirname(path), "patches")
-    actual_patches = []
+    actual_patches: list[str] = []
     if os.path.isdir(patches_path):
         actual_patches.extend(
-            os.path.join(root[len(patches_path)+1:], f)
+            os.path.join(root[len(patches_path) + 1:], f)
             for root, _, files in os.walk(patches_path) for f in files)
     unused_patches = copy.copy(actual_patches)
     for version in parsed.get("patches", []):
@@ -26,7 +26,7 @@ def main(path:str) -> None:
                 f" You should either remove `{version}` from the `patches` section,"
                 f" or add it to the `sources` section"
             )
-        for i, patch in enumerate(patches):
+        for _, patch in enumerate(patches):
             patch_file_name = str(patch["patch_file"])
             if not patch_file_name.startswith("patches/"):
                 print(f"patches should be located in [patches](https://github.com/conan-io/conan-center-index/tree/master/recipes/{patches_path}) subfolder, not in {patch_file_name}")
@@ -39,9 +39,10 @@ def main(path:str) -> None:
 
     if unused_patches:
         print(f"Following patch files are not referenced in [{path}](https://github.com/conan-io/conan-center-index/tree/master/recipes/{path})")
-        for p in unused_patches:
-            print(f"- [{p}](https://github.com/conan-io/conan-center-index/tree/master/recipes/{patches_path}/{p})")
+        for patch in unused_patches:
+            print(f"- [{patch}](https://github.com/conan-io/conan-center-index/tree/master/recipes/{patches_path}/{patch})")
         print("\n")
+    return 0
 
 
 if __name__ == "__main__":
